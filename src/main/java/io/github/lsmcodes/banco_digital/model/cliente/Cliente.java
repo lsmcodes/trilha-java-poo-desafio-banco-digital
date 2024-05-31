233404c7942dba1f1232213351e784b1bc88b2e6
Lorena Medeiros <lomedeiros@proton.me>
2024-05-30 23:26:44 -0300

    feat: add Cliente class for bank client management

---
+++ src/main/java/io/github/lsmcodes/banco_digital/model/cliente/Cliente.java
@@ -0,0 +1,15 @@
+package io.github.lsmcodes.banco_digital.model.cliente;
+
+public class Cliente {
+
+        private String nome;
+
+        public String getNome() {
+                return nome;
+        }
+
+        public void setNome(String nome) {
+                this.nome = nome;
+        }
+
+}
